d49f09470b582ebcefa8283c9d172a6539dbaa7f
StefanyPena <66650035+stefanyPena@users.noreply.github.com>
2020-06-13 22:20:34 -0500

    Update A4.r

--- Script-R files/A4.r
+++ Script-R files/A4.r
@@ -1,9 +1,1 @@
-load.lib <- c('PerformanceAnalytics','xtable','stargazer', 'ggpubr','xts',"readxl","data.table","lubridate",'tidyr','rddensity','estimatr','rdrobust',
-              'dplyr','ggplot2','forecast','RColorBrewer','quadprog','NMOF','fBonds','cvar','mFilter',"x13binary","seasonal","lmtest","RCurl") 
-install.lib <- load.lib[!load.lib %in% installed.packages()]
-for(lib in install.lib) install.packages(lib)
-sapply(load.lib,require,character=TRUE)
 
-url_robust <- "https://raw.githubusercontent.com/IsidoreBeautrelet/economictheoryblog/master/robust_summary.R"
-eval(parse(text = getURL(url_robust, ssl.verifypeer = FALSE)),
-     envir=.GlobalEnv)
